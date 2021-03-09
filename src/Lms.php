093f7e8bc672616b691b420f8ea99fd4accc4f78
Namtq <quocnam@hanoicdc.com>
2021-03-09 09:49:49 +0700

    update lms api

--- src/Lms.php
+++ src/Lms.php
@@ -36,4 +36,39 @@ class Lms
         \Log::error($response->body());
         return false;
     }
+    public function getCourseIncludePrice($params) {
+        $whereArr = \Arr::only($params, ['course_id']);
+        $filter = [];
+        foreach($whereArr as $k => $v){
+            if (is_null($v)) continue;
+            switch ($k) {
+                default:
+                    if (is_array($v)) {
+                        $filter[$k] = ['inq' => $v];
+                    }
+                    else {
+                        $filter[$k] = ['eq' => $v];
+                    }
+                    break;
+            }
+        }
+        
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/courses',['filter' => json_encode([
+            'where' => $filter,
+            'include' => [
+                [
+                    'relation' => 'eduCoursePrices',
+                    'scope' => [
+                        'fields'=> ['course_id','amount','price_id','validation'],
+                        'where' => ['status' => 'active','from_date' => ['lte' => date("Y-m-d")],'to_date' => ['gte' => date('Y-m-d')]]
+                    ]
+                ]
+            ],
+            ])]);
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
 }
