42a0034c46b370f72d1206d7f2923e09cf52d831
diamenvn <x321pm@gmail.com>
2021-03-11 11:44:03 +0700

    Create Payroll.php

---
+++ src/Payroll.php
@@ -0,0 +1,21 @@
+<?php
+namespace Microservices;
+
+
+class Payroll
+{
+    protected $_url;
+    public function __construct() {
+        $this->_url = env('API_MICROSERVICE_URL').'/payroll';
+    }
+
+    public function getAllBenefitWithActive()
+    {
+        $response = \Http::withToken(env('API_MICROSERVICE_TOKEN',''))->get($this->_url.'/benefits/');
+        if ($response->successful()) {
+            return $response->json();
+        }
+        \Log::error($response->body());
+        return false;
+    }
+}
